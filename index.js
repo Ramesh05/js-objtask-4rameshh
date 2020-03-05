e9275631486bf6dbc727c02efb2deb856f127f2c
Ramesh05 <rameshkannan240@gmail.com>
2020-03-05 10:33:56 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,70 @@
-// Import stylesheets
-import './style.css';
+const obj={
+  id:3,
+  name:'ramesh',
+  class:'ece',
+  age:22,
+  dob:22/12/1996,
+  location:'france',
+  getName:function(name){
+         return  this.name;
+  },
+setName:function(name){
+  this.name=name;
+  },
+ getid:function(id){
+         return  this.id;   
+         },
+setid:function(id){
+  this.id=id;
+  },
+getclass:function(){
+         return  this.class;   
+         },
+setclass:function(class1){
+  this.class= class1;
+  },
+  getage: function(){
+       return this.name;   
+       },
+setage:function(name){
+  this.name=name;
+  },
+  getdob:function(){
+    return this.name;
+    },
+    setdob:function(name) {
+        this.name=name;
+     },
+getlocation:function(){
+      return this.name;
+                 },
+      setlocation:function(name){
+        this.name=name;
+      }                
+  }
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let name=obj.getName();
+console.log('name:',name);
+obj.setName('hello');
+name=obj.getName();
+console.log('name:',name);
+
+
+ 
+let id=obj.getid();
+console.log('id:',id);
+obj.setid(50);
+id=obj.getid();
+console.log('id:',id);
+
+
+
+console.log(obj.setclass(7))
+console.log(obj.getclass())
+console.log(obj.setage(23))
+console.log(obj.getage())
+console.log(obj.setdob('22/12/1996'))
+console.log(obj.getdob())
+console.log(obj.setlocation('japan'))
+console.log(obj.getlocation())
